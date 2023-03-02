7412c86390cb061434c21566a54dad96d3b60392
Isha <67155726+ishageens@users.noreply.github.com>
2023-03-02 14:49:45 +0100

    Init

---
+++ SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
